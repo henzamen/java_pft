8afc664ec104f027fe93b79e9b27448b2501161e
hsamenho <henrich.samenhof@t-systems.com>
2021-06-06 00:39:58 +0300

    Задание_03.Первые тесты.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,12 @@
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProgram{
 
   public static void main(String[] args) {	  
+
+      String somebody = "world";
       System.out.println("Hello, world!");
+
   }
 
 }
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static ru.stqa.pft.sandbox.MyPointProgram.distance;
+
+
+public class PointTests {
+
+    @Test
+    public void testTwoPointsDistanceMethod() {
+
+        Point p1 = new Point(5.2,-2.0);
+        Point p2 = new Point(-7.75,8);
+        Assert.assertEquals(p1.distance(p2),16.361616668288008);
+
+    }
+
+    @Test
+    public void testTwoPointsDistanceFunction() {
+
+        Point p1 = new Point(5.2,-2.0);
+        Point p2 = new Point(-7.75,8);
+        Assert.assertEquals(distance(p1,p2),16.361616668288008);
+
+    }
+
+    @Test
+    public void testTwoPointsDistanceFunctionNegative() {
+
+        Point p1 = new Point(5.2,-2.0);
+        Point p2 = new Point(-7.75,8);
+        Assert.assertNotEquals(distance(p1,p2),16);
+
+    }
+
+    @Test
+    public void testTwoPointsDistanceMethodTrue() {
+
+        Point p1 = new Point(5.2,-2.0);
+        Point p2 = new Point(-7.75,8);
+        Assert.assertTrue(p1.distance(p2) == 16.361616668288008);
+
+    }
+
+}
