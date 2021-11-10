46a0b282e773ad4936b19cd09999b982ea7de97d
hsamenho <henrich.samenhof@t-systems.com>
2021-11-11 00:25:19 +0300

    Задание#16.Исправлено.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInGroup.java
@@ -15,37 +15,32 @@ public class ContactInGroup extends TestBase {
 
     Groups groups;
     GroupData targetGroup;
-    int lastGroupValue;
+    int targetGroupValue;
     Contacts contacts;
     ContactData modifiedContact;
-    int lastContactId;
+    int targetContactId;
 
     @BeforeMethod
     public void setPreconditions() {
+        //check if any group exists
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            String groupName = RandomGenerator.getRandomString(5);
+            app.getGroups().create(new GroupData()
+                    .withName(groupName)
+                    .withHeader("header " + groupName)
+                    .withFooter("footer " + groupName));
 
-        app.goTo().clickLinkHome();
-        app.getContacts().clickAddNew();
-        app.getContacts().modify(new ContactData()
-                        .withFirstname(RandomGenerator.getRandomName(5))
-                        .withLastname(RandomGenerator.getRandomName(5))
-                        .withNickname(RandomGenerator.getRandomString(5)),
-                true);
-        app.goTo().groupPage();
-        String groupName = RandomGenerator.getRandomString(5);
-        app.getGroups().create(new GroupData()
-                .withName(groupName)
-                .withHeader("header " + groupName)
-                .withFooter("footer " + groupName));
+        }
+        //check if any contact exists
+        if (app.db().contacts().size() == 0) {
+            createContact();
+        }
     }
 
     @Test
     public void addContactToGroup() {
-        groups = app.db().groups();
-        targetGroup = app.getGroups().getLastGroup(groups);
-        lastGroupValue = targetGroup.getValue();
-        contacts = app.db().contacts();
-        modifiedContact = app.getContacts().getLastContact(contacts);
-        lastContactId = modifiedContact.getId();
+        getContactWithoutGroup();
 
         app.goTo().clickLinkHome();
         app.getContacts().addToGroup(modifiedContact, targetGroup);
@@ -53,8 +48,8 @@ public class ContactInGroup extends TestBase {
 
         contacts = app.db().contacts();
         groups = app.db().groups();
-        modifiedContact = app.getContacts().getContactById(contacts, lastContactId);
-        targetGroup = app.getGroups().getGroupByValue(groups, lastGroupValue);
+        modifiedContact = app.getContacts().getContactById(contacts, targetContactId);
+        targetGroup = app.getGroups().getGroupByValue(groups, targetGroupValue);
 
         Assert.assertTrue(app.getContactHelper().isContactInGroup(modifiedContact, targetGroup));
 
@@ -62,15 +57,77 @@ public class ContactInGroup extends TestBase {
 
     @Test
     public void removeContactFromGroup() {
+        getContactWithGroup();
+
         app.goTo().clickLinkHome();
         app.getContactHelper().removeContactFromGroup(modifiedContact, targetGroup);
         contacts = app.db().contacts();
         groups = app.db().groups();
-        modifiedContact = app.getContacts().getContactById(contacts, lastContactId);
-        targetGroup = app.getGroups().getGroupByValue(groups, lastGroupValue);
+        modifiedContact = app.getContacts().getContactById(contacts, targetContactId);
+        targetGroup = app.getGroups().getGroupByValue(groups, targetGroupValue);
 
         Assert.assertFalse(app.getContactHelper().isContactInGroup(modifiedContact, targetGroup));
-
     }
 
+    ///////////////////////////////PRIVATE METHODS USED//////////////////////////////////////
+
+    private void getContactWithoutGroup() {
+        contacts = app.db().contacts();
+        groups = app.db().groups();
+        targetContactId = 0;
+        search:
+        for (ContactData contact : contacts) {
+            for (GroupData group : groups) {
+                if (!app.getContacts().isContactInGroup(contact, group)) {
+                    modifiedContact = contact;
+                    targetGroup = group;
+                    targetContactId = contact.getId();
+                    targetGroupValue = group.getValue();
+                    break search;
+                }
+            }
+
+        }
+        if (targetContactId == 0) {
+            createContact();
+            groups = app.db().groups();
+            targetGroup = app.getGroups().getLastGroup(groups);
+            targetGroupValue = targetGroup.getValue();
+        }
+    }
+
+    private void createContact() {
+        app.goTo().clickLinkHome();
+        app.getContacts().clickAddNew();
+        app.getContacts().modify(new ContactData()
+                        .withFirstname(RandomGenerator.getRandomName(5))
+                        .withLastname(RandomGenerator.getRandomName(5))
+                        .withNickname(RandomGenerator.getRandomString(5)),
+                true);
+        contacts = app.db().contacts();
+        modifiedContact = app.getContacts().getLastContact(contacts);
+        targetContactId = modifiedContact.getId();
+    }
+
+    private void getContactWithGroup() {
+        contacts = app.db().contacts();
+        groups = app.db().groups();
+        targetContactId = 0;
+        search:
+        for (ContactData contact : contacts) {
+            for (GroupData group : groups) {
+                if (app.getContacts().isContactInGroup(contact, group)) {
+                    modifiedContact = contact;
+                    targetGroup = group;
+                    targetContactId = contact.getId();
+                    targetGroupValue = group.getValue();
+                    break search;
+                }
+            }
+
+        }
+        if (targetContactId == 0) {
+            addContactToGroup();
+        }
+    }
 }
