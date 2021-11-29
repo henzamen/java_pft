527bc27742ed0434c2863476ce3454707fe562d7
hsamenho <henrich.samenhof@t-systems.com>
2021-11-29 20:31:43 +0300

    Platform.try.4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -59,8 +59,7 @@ public class ApplicationManager {
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            //capabilities.setPlatform(Platform.fromString(System.getProperty("platform","WIN10")));
-            capabilities.setPlatform(Platform.WIN10);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform","win10")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
 
 
